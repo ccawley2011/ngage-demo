cc9e8d19b7eeacd424a4eb86437892b9797229cd
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-02-18 22:38:25 +0100

    Update demo to use SDL2's rendering API (software renderer)

--- src/demo.c
+++ src/demo.c
@@ -2,15 +2,16 @@
 
 #include <stdlib.h>
 #include <SDL.h>
-#include <dbgprint.h>
 
 int main(int argc, char *argv[])
 {
-    int          status     = EXIT_SUCCESS;
-    SDL_bool     is_running = SDL_TRUE;
-    SDL_Surface *splash;
-    SDL_Surface *screen;
-    SDL_Window  *window;
+    int           status     = EXIT_SUCCESS;
+    SDL_bool      is_running = SDL_TRUE;
+    SDL_Renderer *renderer;
+    SDL_Surface  *tempsf;
+    SDL_Surface  *screen;
+    SDL_Texture  *splash;
+    SDL_Window   *window;
 
     (void)argc;
     (void)argv;
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
 
     if (0 != SDL_Init(SDL_INIT_VIDEO))
     {
-        dbgprint("Unable to initialise SDL: %s", SDL_GetError());
+        SDL_Log("Unable to initialise SDL: %s", SDL_GetError());
         status = EXIT_FAILURE;
         goto error;
     }
@@ -30,53 +31,113 @@ int main(int argc, char *argv[])
         SDL_WINDOWPOS_UNDEFINED,
         176, 208,
         SDL_WINDOW_FULLSCREEN);
-
     if (NULL == window)
     {
-        dbgprint("Could not create window: %s", SDL_GetError());
+        SDL_Log("Could not create window: %s", SDL_GetError());
         status = EXIT_FAILURE;
         goto error;
     }
+
+    renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_SOFTWARE);
+    if (NULL == renderer)
+    {
+        SDL_Log("Could not create: %s", SDL_GetError());
+        status = EXIT_FAILURE;
+        goto error;
+    }
+    if (0 != SDL_RenderSetIntegerScale(renderer, SDL_TRUE))
+    {
+        SDL_Log("Could not enable integer scale: %s", SDL_GetError());
+    }
+
+    tempsf = SDL_LoadBMP("E:\\splash.bmp");
+    if (NULL == tempsf)
+    {
+        SDL_Log("Failed to load image: %s", SDL_GetError());
+        status = EXIT_FAILURE;
+        goto error;
+    }
+
+    splash = SDL_CreateTextureFromSurface(renderer, tempsf);
+    if (NULL == splash)
+    {
+        SDL_Log("Failed to create texture from surface: %s", SDL_GetError());
+        status = EXIT_FAILURE;
+        goto error;
+    }
+    SDL_FreeSurface(tempsf);
+
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
 
     screen = SDL_GetWindowSurface(window);
     if (NULL == screen)
     {
-        dbgprint("Could not get window surface: %s", SDL_GetError());
-        status = EXIT_FAILURE;
-        goto error;
-    }
-
-    splash = SDL_LoadBMP("E:\\splash.bmp");
-    if (NULL == splash)
-    {
-        dbgprint("Failed to load image: %s", SDL_GetError());
+        SDL_Log("Could not get window surface: %s", SDL_GetError());
         status = EXIT_FAILURE;
         goto error;
     }
 
     while(is_running)
     {
-        SDL_Event event;
+        SDL_Event        event;
+        SDL_RendererFlip flip;
+        double           angle;
 
         if (SDL_PollEvent(&event))
         {
             switch (event.key.keysym.sym)
             {
-                case SDLK_ESCAPE:
+                case SDLK_5:
+                    flip = SDL_FLIP_NONE;
+                    break;
+                case SDLK_7:
+                    flip = SDL_FLIP_HORIZONTAL;
+                    break;
+                case SDLK_UP:
+                    angle = 0.f;
+                    break;
+                case SDLK_DOWN:
+                    angle = 180.f;
+                    break;
+                case SDLK_LEFT:
+                    angle = 270.f;
+                    break;
+                case SDLK_RIGHT:
+                    angle = 90.f;
+                    break;
+                case SDLK_BACKSPACE:
                     is_running = SDL_FALSE;
                     break;
                 default:
+                    angle = 0.f;
                     break;
             }
         }
 
-        SDL_BlitSurface(splash, NULL, screen, NULL);
-        SDL_UpdateWindowSurface(window);
+        if (0 != SDL_RenderCopyEx(renderer, splash, NULL, NULL, angle, NULL, flip))
+        {
+            SDL_Log("Could not render texture: %s", SDL_GetError());
+            goto error;
+        }
+        SDL_RenderPresent(renderer);
     }
 
 error:
-    SDL_FreeSurface(splash);
-    SDL_FreeSurface(screen);
+    if (screen)
+    {
+        SDL_FreeSurface(screen);
+    }
+
+    if (splash)
+    {
+        SDL_DestroyTexture(splash);
+    }
+
+    if (renderer)
+    {
+        SDL_DestroyRenderer(renderer);
+    }
 
     if (window)
     {
